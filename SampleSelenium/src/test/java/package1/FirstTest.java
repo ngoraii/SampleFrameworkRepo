c8631327d3f6d0000edb19671c9828f1b1694ac4
Nayan Gorai <ngorai17@gmail.com>
2025-02-17 19:00:51 +0530

    push

--- SampleSelenium/src/test/java/package1/FirstTest.java
+++ SampleSelenium/src/test/java/package1/FirstTest.java
@@ -5,8 +5,10 @@ import org.testng.annotations.Test;
 public class FirstTest {
 	@Test
 	public void firstTest() {
-		System.out.println("package1Test1");
-	}
+		String url=System.getProperty("url","default url");
+		String browser=System.getProperty("browser","default browser");
+		System.out.println("URL is : "+url);
+		System.out.println("Browser is : "+browser);	}
 	@Test
 	public void secondTest() {
 		System.out.println("package1Test2");
--- SampleSelenium/src/test/java/package2/SecondTest.java
+++ SampleSelenium/src/test/java/package2/SecondTest.java
@@ -5,7 +5,10 @@ import org.testng.annotations.Test;
 public class SecondTest {
 	@Test
 	public void thirdtTest() {
-		System.out.println("package2Test1");
+		String url=System.getProperty("url","default url");
+		String browser=System.getProperty("browser","default browser");
+		System.out.println("URL is : "+url);
+		System.out.println("Browser is : "+browser);
 	}
 	@Test
 	public void fourthTest() {
