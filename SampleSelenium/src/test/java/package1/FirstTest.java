f033873c248523e9959203e0c700cfb5a4ec513e
Nayan Gorai <ngorai17@gmail.com>
2025-02-17 18:23:43 +0530

    push

--- SampleSelenium/src/test/java/package1/FirstTest.java
+++ SampleSelenium/src/test/java/package1/FirstTest.java
@@ -5,11 +5,11 @@ import org.testng.annotations.Test;
 public class FirstTest {
 	@Test
 	public void firstTest() {
-		System.out.println("package1Test1");
+		System.out.println("package1firstTest");
 	}
 	@Test
 	public void secondTest() {
-		System.out.println("package1Test2");
+		System.out.println("package1secondTest");
 	}
 	
 
--- SampleSelenium/src/test/java/package2/SecondTest.java
+++ SampleSelenium/src/test/java/package2/SecondTest.java
@@ -5,11 +5,11 @@ import org.testng.annotations.Test;
 public class SecondTest {
 	@Test
 	public void thirdtTest() {
-		System.out.println("package2Test1");
+		System.out.println("package2thirdTest");
 	}
 	@Test
 	public void fourthTest() {
-		System.out.println("package2Test2");
+		System.out.println("package2fourthTest");
 	}
 
 }
