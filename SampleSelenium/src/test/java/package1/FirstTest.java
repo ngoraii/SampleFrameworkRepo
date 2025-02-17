82f37b110c121fc3bfd65c14e28cfc0cd1b310e2
Nayan Gorai <ngorai17@gmail.com>
2025-02-17 17:43:18 +0530

    sample

---
+++ SampleSelenium/src/test/java/package1/FirstTest.java
@@ -0,0 +1,15 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	@Test
+	public void firstTest() {
+		System.out.println("package1Test1");
+	}
+	public void secondTest() {
+		System.out.println("package1Test2");
+	}
+	
+
+}
---
+++ SampleSelenium/src/test/java/package2/SecondTest.java
@@ -0,0 +1,14 @@
+package package2;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+	@Test
+	public void thirdtTest() {
+		System.out.println("package2Test1");
+	}
+	public void fourthTest() {
+		System.out.println("package2Test2");
+	}
+
+}
